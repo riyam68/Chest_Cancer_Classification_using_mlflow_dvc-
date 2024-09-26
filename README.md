5e9154b859b1e56249bd3dc3b74eb8518ea17f6a
riyam68 <riyamsarfaraz@gmail.com>
2024-09-26 16:30:02 +0500

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Chest_Cancer_Classification_using_mlflow_dvc-
+
+
+# Workflows
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update param.yaml
+4. update the entity
+5. update the configuration manager in src config
+6. update the components
+7. update the pipeline
+8. update the main.py
+9. update the dvc.yaml
--- template.py
+++ template.py
@@ -6,7 +6,8 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "cnnClassifier"
 
-list_of_files = [".github/workflows/.gitkeep",f"src/cnnClassifier/__init__.py",
+list_of_files = [".github/workflows/.gitkeep",
+    f"src/cnnClassifier/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
