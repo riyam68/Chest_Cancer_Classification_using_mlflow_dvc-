7cd893b5f3a5569dddff6a0d37b12d9320e791ec
riyam68 <riyamsarfaraz@gmail.com>
2024-09-30 10:42:41 +0500

    prepare base model (correction Update)

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -31,11 +31,11 @@ class ConfigurationManager:
             root_dir= Path(config.root_dir),
             base_model_path = Path(config.base_model_path),
             updated_base_model_path = Path(config.updated_base_model_path),
-            params_image_size = self.params.IMAGE_SIZE,
-            params_learning_rate= self.params.LEARNING_RATE,
-            params_include_top = self.params.INCLUDE_TOP,
-            params_weights= self.params.WEIGHTS,
-            params_classes = self.params.CLASSES
+            params_image_size = self.param.IMAGE_SIZE,
+            params_learning_rate= self.param.LEARNING_RATE,
+            params_include_top = self.param.INCLUDE_TOP,
+            params_weights= self.param.WEIGHTS,
+            params_classes = self.param.CLASSES
         )
 
         return prepare_base_model_config
