80a2619fb142ccfc293f67a9ae7d1ab313ad2e5c
riyam68 <riyamsarfaraz@gmail.com>
2024-09-30 17:08:45 +0500

    model trainer added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,7 @@
+import os
 from src.cnnClassifier.constant import *
 from src.cnnClassifier.utils.common import read_yaml, create_directories
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAM_FILE_PATH):
@@ -22,6 +23,9 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
+
+
+
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
 
@@ -39,3 +43,25 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        preparing_base_model = self.config.prepare_base_model
+        params = self.param
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"Chest-CT-Scan-data")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir = Path(training.root_dir),
+            trained_model_path = Path(training.trained_model_path),
+            updated_base_model_path = Path(preparing_base_model.updated_base_model_path),
+            training_data = Path(training_data),
+            params_epochs = params.EPOCHS,
+            params_batch_size = params.BATCH_SIZE,
+            params_is_augmentation = params.AUGMENTATION,
+            params_image_size = params.IMAGE_SIZE
+        )
+
+        return training_config
